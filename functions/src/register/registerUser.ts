4493779af1e75f15ca3b88595b017f9bfdb84a58
Aamish Ahmad Beg <AcnoSaga@Akhlaks-MacBook-Pro.local>
2020-10-17 13:32:52 +0530

    registerUser function complete

--- functions/src/register/registerUser.ts
+++ functions/src/register/registerUser.ts
@@ -1,13 +1,30 @@
 import * as functions from 'firebase-functions';
-import { authHandler } from '../handlers/auth';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
 import { stringDataValidator } from '../handlers/stringData';
 
 export const registerUser =  functions.https.onCall(async (data, context) => {
     try {
-        authHandler(context);
-        stringDataValidator(data.path, data.body);
+        // authHandler(context);
+        stringDataValidator(data.displayName, data.password ,data.email, data.type);
     } catch (err) {
         throw err;
     }
-
+    const userResult =  await admin.auth().createUser({
+        email: data.email,
+        emailVerified: false,
+        password: data.password,
+        displayName: data.displayName,
+        disabled: false
+      });
+    
+    const userRef =  await admin.firestore().collection('users').add({
+        email: userResult.email,
+        displayName: userResult.displayName,
+        type: data.type,
+    });
+    
+    return await userRef.get();
   });
