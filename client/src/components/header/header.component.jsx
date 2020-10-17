3d38ee137b05bbe7e805158b1e9a216eceec5271
Aamish Ahmad Beg <AcnoSaga@Akhlaks-MacBook-Pro.local>
2020-10-18 03:25:49 +0530

    Some work done

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,44 +1,63 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { createStructuredSelector } from "reselect";
-import Logo from "../../assets/logo.png";
-import { selectCurrentUser } from "../../redux/user/user.selectors";
-import "./header.component.styles.css";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
+import Logo from '../../assets/logo.png';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import './header.component.styles.css';
 
 function Header({ currentUser }) {
-  return (
-    <div id="header">
-      <Link to="/" className="logo">
-        <img src={Logo} />
-      </Link>
+	console.log(currentUser);
+	return (
+		<div id='header'>
+			<Link to='/' className='logo'>
+				<img src={Logo} />
+			</Link>
 
-      <div className="titles">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/team">Our Team</Link>
-          </li>
+			<div className='titles'>
+				<ul>
+					{/* {currentUser == null ? (
+						<div>
+							<li>
+								<Link to='/'>Home</Link>
+							</li>
+							<li>
+								<Link to='/team'>Our Team</Link>
+							</li>
+							<li>
+								<Link to='/account'>Sign In</Link>
+							</li>
+						</div>
+					) : */}
+					(
+					<div>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/team'>Our Team</Link>
+						</li>
 
-          <li>
-            <Link to="/work">Work</Link>
-          </li>
-          <li>
-            <Link to="/jobs">Your Jobs</Link>
-          </li>
-          <li>
-            <Link to="/account">Account</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+						<li>
+							<Link to='/work'>Work</Link>
+						</li>
+						<li>
+							<Link to='/jobs'>Your Jobs</Link>
+						</li>
+						<li>
+							<Link to='/account'>Account</Link>
+						</li>
+					</div>
+					)
+          {/* } */}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
+	currentUser: selectCurrentUser,
 });
 
 export default connect(mapStateToProps)(Header);
