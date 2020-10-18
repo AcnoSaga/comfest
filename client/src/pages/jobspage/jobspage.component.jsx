e4d925864bdadd55641f273823e247c8e752633f
saad khan <saadgamer124@gmail.com>
2020-10-18 06:05:27 +0530

    Stabalised Registeration forms and set up modal

--- client/src/pages/jobspage/jobspage.component.jsx
+++ client/src/pages/jobspage/jobspage.component.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+} from "reactstrap";
 
-function JobsPage() {
-  return <h1>JobsPage</h1>;
-}
+const JobsPage = (props) => {
+  const { buttonLabel, className } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle}>
+        {buttonLabel}
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Create Job</ModalHeader>
+        <ModalBody>
+          <Form></Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Submit
+          </Button>
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
 
 export default JobsPage;
